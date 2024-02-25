fa302b91a6339909270e25b60d7ce0c2b52ba023
uaianaluiza <analuiza101@hotmail.com>
2024-02-25 14:35:54 -0300

    config

---
+++ src/main/kotlin/com/santa/hamburgueria/HamburgueriaApplication.kt
@@ -0,0 +1,11 @@
+package com.santa.hamburgueria
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class HamburgueriaApplication
+
+fun main(args: Array<String>) {
+    runApplication<HamburgueriaApplication>(*args)
+}
