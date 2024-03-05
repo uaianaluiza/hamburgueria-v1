56955e42b7f107bba2e2d9e78b79872a621de608
uaianaluiza <analuiza101@hotmail.com>
2024-03-05 08:42:07 -0300

    rename

--- src/main/kotlin/com/santa/hamburgueria/HamburguerApplication.kt
+++ src/main/kotlin/com/santa/hamburgueria/HamburguerApplication.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class HamburgueriaApplication
+class HamburgerApplication
 
 fun main(args: Array<String>) {
-    runApplication<HamburgueriaApplication>(*args)
+    runApplication<HamburgerApplication>(*args)
 }
--- src/test/kotlin/com/santa/hamburgueria/HamburgerApplicationTests.kt
+++ src/test/kotlin/com/santa/hamburgueria/HamburgerApplicationTests.kt
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test
 import org.springframework.boot.test.context.SpringBootTest
 
 @SpringBootTest
-class HamburgueriaApplicationTests {
+class HamburgerApplicationTests {
 
 	@Test
 	fun contextLoads() {
